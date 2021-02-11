75c1a9249db2dee39c25ca6a627c0fdfeab95ac7
athomas-wtv <athomas.wtv@gmail.com>
2021-02-10 22:59:49 -0500

    Added Smallest amount method

--- Program.cs
+++ Program.cs
@@ -54,7 +54,46 @@ namespace Sites
                 }
             }
 
+            DisplayLargestTransaction(master);
+            DisplaySmallestTransaction(master);
             
+        }
+
+        private static void DisplaySmallestTransaction(Dictionary<int, Transaction> master)
+        {
+            var hasSmallest = 1;
+            var currentAmount = 0.00;
+            var smallestAmount = master[hasSmallest].Amount;
+            for(var i = 1; i <= master.Count; i++)
+            {
+                currentAmount = master[hasSmallest].Amount;
+                if(currentAmount < smallestAmount)
+                {
+                    hasSmallest = i;
+                }
+            }
+            Display(master[hasSmallest]);
+        }
+
+        private static void DisplayLargestTransaction(Dictionary<int, Transaction> master)
+        {
+            var hasLargest = 1;
+            var currentAmount = 0.00;
+            var largestAmount = master[hasLargest].Amount;
+            for(var i = 1; i <= master.Count; i++)
+            {
+                currentAmount = master[hasLargest].Amount;
+                if(currentAmount > largestAmount)
+                {
+                    hasLargest = i;
+                }
+            }
+            Display(master[hasLargest]);
+        }
+
+        private static void Display(Transaction transaction)
+        {
+            Console.WriteLine("Date: {0}, Description: {1}, Amount: {2}, Account Name: {3}", transaction.Date, transaction.Description, transaction.Amount, transaction.AccountName);
         }
     }
 }
