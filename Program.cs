215b88c993411a9398a49254bf6ea4ab059976d3
athomas-wtv <athomas.wtv@gmail.com>
2021-02-10 22:31:46 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strTransactions = "";
+            var transactions = new List<Transaction>();
+            var transaction = new Transaction();
+
+            Type type = typeof(Transaction);
+            var numOfProperties = type.GetProperties().Length;
+            var properties = type.GetProperties();
+            
+            var propertyNames = new List<string>();
+            foreach(var prop in properties)
+            {
+                propertyNames.Add(prop.Name);
+            }
+
+            var fileItems = strTransactions.Split(',');
+            int num = 0;
+            for (int i = numOfProperties; i <= fileItems.Length; i++)
+            {
+                num = i - numOfProperties;
+                transaction.GetType().GetProperty(propertyNames[num]).SetValue(transaction, fileItems[i]);
+                transactions.Add(transaction);
+            }
+
+            var master = new Dictionary<int, Transaction>();
+            var index = 1;
+            foreach(var t in transactions)
+            {
+                master.Add(index,t);
+            }
+            
+            bool first = true;
+            foreach(var t in transactions)
+            {
+                foreach(var tr in master)
+                {
+                    var isEqual = t.Equals(tr.Value);
+                    if(isEqual && first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        master.Remove(tr.Key);
+                    }
+
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,16 @@
+using System;
+public class Transaction
+{
+    public DateTime Date {get; set;}
+    public string Description { get; set; }
+    public string OriginalDescription { get; set; }
+    public decimal Amount { get; set; }
+    public string TransactionType { get; set; }
+    public string Category { get; set; }
+    public string AccountName { get; set; }
+    public string Labels { get; set; }
+    public string Notes { get; set; }
+}
+
+    
+// Date,Description,Original Description,Amount,Transaction Type,Category,Account Name,Labels,Notes
