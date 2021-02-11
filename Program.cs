2395fa890bc738bc7a1543cf6a94c996683dd5a7
athomas-wtv <athomas.wtv@gmail.com>
2021-02-10 23:55:22 -0500

    Fixed type property assignment issues

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sites
 {
@@ -7,29 +8,55 @@ namespace Sites
     {
         static void Main(string[] args)
         {
-            String strTransactions = "";
-            var transactions = new List<Transaction>();
-            var transaction = new Transaction();
-
+            // Use reflection to get the number of properties for the class.
+            Console.WriteLine("Use reflection to get the number of properties for the class");
             Type type = typeof(Transaction);
             var numOfProperties = type.GetProperties().Length;
             var properties = type.GetProperties();
             
+            // Add property names to a list of strings
+            Console.WriteLine("Add property names to a list of strings");
             var propertyNames = new List<string>();
             foreach(var prop in properties)
             {
                 propertyNames.Add(prop.Name);
             }
 
-            var fileItems = strTransactions.Split(',');
-            int num = 0;
-            for (int i = numOfProperties; i <= fileItems.Length; i++)
+            // Converting each record to be a transaction object and adding it to a list of transactions.
+            Console.WriteLine("Converting each record to be a transaction object and adding it to a list of transactions.");
+            int propertyPlacement = 0;
+            var transaction = new Transaction();
+            var transactions = new List<Transaction>();
+            string[] rawTransactions = System.IO.File.ReadAllLines("transactions.txt");
+            string[] rtArr;
+            foreach(var rt in rawTransactions.Skip(1))
             {
-                num = i - numOfProperties;
-                transaction.GetType().GetProperty(propertyNames[num]).SetValue(transaction, fileItems[i]);
+                rtArr = rt.Split(",");
+                for (int i = 0; i < rtArr.Length; i++)
+                {
+                    // propertyPlacement = i - numOfProperties;
+                    var currentPropety = transaction.GetType().GetProperty(propertyNames[i]);
+
+                    // Checking the property type then updating the value
+                    if(currentPropety.PropertyType.Name == "String")
+                    {
+                        currentPropety.SetValue(transaction, rtArr[i]);
+                    }
+                    else if(currentPropety.PropertyType.Name == "Double")
+                    {
+                        currentPropety.SetValue(transaction, Convert.ToDouble(rtArr[i]));
+                    }
+                    else if(currentPropety.PropertyType.Name == "DateTime")
+                    {
+                        currentPropety.SetValue(transaction, DateTime.Parse(rtArr[i]));
+                    }
+
+                }
                 transactions.Add(transaction);
             }
 
+            // Adding transactions to a dictionary
+            Console.WriteLine("Adding transactions to a dictionary");
             var master = new Dictionary<int, Transaction>();
             var index = 1;
             foreach(var t in transactions)
@@ -37,25 +64,31 @@ namespace Sites
                 master.Add(index,t);
             }
             
-            bool first = true;
+
+            // Find duplicates and remove them
+            Console.WriteLine("Find duplicates and remove them");
+            bool firstOccurance = true;
             foreach(var t in transactions)
             {
                 foreach(var tr in master)
                 {
                     var isEqual = t.Equals(tr.Value);
-                    if(isEqual && first)
+                    if(isEqual && firstOccurance)
                     {
-                        first = false;
+                        firstOccurance = false;
                     }
-                    else
+                    else if(isEqual)
                     {
                         master.Remove(tr.Key);
                     }
                 }
             }
-
+            
+            Console.WriteLine("Largest Number");
             DisplayLargestTransaction(master);
+            Console.WriteLine("Smallest Number:");
             DisplaySmallestTransaction(master);
+            Console.ReadLine();
             
         }
 
